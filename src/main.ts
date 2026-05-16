526f9ea6db7ee2f850e02da71eab6fedb157ecf9
LilyDarkOv3r <lili.suveges.1002@gmail.com>
2026-05-17 00:26:34 +0200

    spagettikód but functional(néha)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ const tempP = document.querySelector("#temp") as HTMLParagraphElement;
 const rainP = document.querySelector("#esomenny") as HTMLParagraphElement;
 const searchedCity = document.querySelector("#citysearch") as HTMLInputElement;
 const searchBTN = document.querySelector("#searchBTN") as HTMLButtonElement;
+const background = document.querySelector("#background") as HTMLDivElement;
 
 //api teszt
 
@@ -18,26 +19,72 @@ async function getWeather(lat: number, lon: number) { // async a varakozos fuggv
    
     tempP.innerHTML = `Hőmérséklet: ${weatherData.current.temperature_2m}°C`; //kulon fuggvenybe kellene majd rakni a requestbol
     rainP.innerHTML = `Csapadékmennyiség: ${weatherData.current.rain} mm/h`;
+
+    return weatherData; //hogy a backgroundswitchnek meglegyen
 }
 getWeather(47.68, 17.63);
 
+//keresés
 async function searchCity(city: string) {
     const cityRequest = await fetch(
         `https://geocoding-api.open-meteo.com/v1/search?name=${city}`);
 
         const cityData = await cityRequest.json();
-        return cityData.results;
+        return cityData;
 }
 
-searchBTN!.addEventListener("click", async () => {
-    const cityData = await searchCity(searchedCity.value);
-    if (!cityData.results) { //csak mert hisztizik a konzol
+searchBTN.addEventListener("click", async () => {
+    const cityData =await searchCity(searchedCity.value);
 
-    alert("Nem található ilyen város");
-
-    return;
+    if (!cityData || cityData.results.length === 0) {
+        alert("Nem található ilyen város");
+        return;
     }
-    const result = cityData.results[0]; //nulladik elem a legjobb talalat, de akar folyamatosan frissites lehetne a talalatokbol egy listaval ala
+    const result = cityData.results[0];
+    const weatherData = await getWeather(result.latitude, result.longitude);
 
-    getWeather(result.latitude, result.longitude);
+    backgroundSwitch(
+        weatherData.current.weather_code
+    );
+
 });
+
+
+//background kep
+
+function backgroundSwitch(weatherCode: number) {
+ switch (true) {
+    
+    case weatherCode === 0:
+        background.style.backgroundImage = "url('src/sunny.avif')";
+        break;
+    
+    case weatherCode <= 48: 
+        background.style.backgroundImage = "url('src/partly-cloudy.avif')";
+        break;
+    
+    case weatherCode <= 57:
+        background.style.backgroundImage = "url('src/cloudy.avif')";
+        break;
+    
+    case weatherCode <= 67:
+        background.style.backgroundImage = "url('src/rainy.avif')";
+        break;
+
+    case weatherCode <= 77:
+        background.style.backgroundImage = "url('src/snowy.avif')";
+        break;
+
+    default:
+        background.style.backgroundImage = "url('src/default.avif')";
+
+ }   
+}
+
+backgroundSwitch(
+    weatherData.current.weather_code
+);
+
+tempP.innerHTML =
+    `Hőmérséklet:
+    ${weatherData.current.temperature_2m}°C`;
